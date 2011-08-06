42aeae2f3db2718f0ce542b9ed650ecb49f1514a
Gavin Staniforth <gavshouse90@gmail.com>
2011-08-06 06:17:20 +0100

    Added Multi Support

--- Curl.php
+++ Curl.php
@@ -1,24 +1,25 @@
 <?php
 	class Curl
 	{
-		private $_curl, $_isResource, $_isMulti;
+		private $_curl, $_isMulti;
 	
 		/**
 		* Constructor, creates a new CURL resource, with option to pass opts
+		* @param bool $mutli
 		*/
-		public function __construct(array $opts=null, $multi=false)
+		public function __construct($multi=false)
 		{
-			if($multi){
-				$this->_curl = @curl_multi_init();
-			}else{
-				$this->_curl = @curl_init();
-			}
-			
-		
-			if(is_resource($this->_curl)){
-				$this->_isResource 	= ( bool ) true;
-				$this->_isMulti 	= ( bool ) $multi;
-				return;
+			if(function_exists('curl_init')){		
+				if($multi){
+					$this->_curl = @curl_multi_init();
+				}else{
+					$this->_curl = @curl_init();
+				}
+						
+				if(is_resource($this->_curl)){
+					$this->_isMulti 	= ( bool ) $multi;
+					return;
+				}			
 			}
 			
 			throw new Exception('Failed to create CURL Resource, is CURL installed ?');
@@ -29,12 +30,10 @@
 		*/		
 		public function __destruct()
 		{
-			if ($this->_isResource) {
-				if($this->_isMulti){
-					curl_multi_close($this->_curl);
-				}else{
-					curl_close($this->_curl);
-				}
+			if($this->_isMulti){
+				curl_multi_close($this->_curl);
+			}else{
+				curl_close($this->_curl);
 			}
 		}
 		
@@ -43,12 +42,119 @@
 		*/		
 		public function __clone()
 		{
-			if ($this->_isResource) {
-				$this->_curl = curl_copy_handle($this->_curl);
-				return;
+			$this->_curl = curl_copy_handle($this->_curl);
+			return;
+		}
+		
+		/**
+		* Used for debugging
+		*/
+		public function __toString()
+		{
+			return print_r($this->_curl, true);
+		}
+		
+		/**
+		* Used for method overloading on _setOpt and _setOptArray
+		*/
+		public function __call($method, $args)
+		{
+			if($method === 'setOpt'){
+				if (is_array($args[0])) {
+					return call_user_func_array(array($this, '_setOptArray'), $args);
+				}
+				
+				return call_user_func_array(array($this, '_setOpt'), $args);
 			}
 			
-			throw new Exception('Trying to clone an empty CURL Object.');
+			throw new BadMethodCallException($method . ' method does not exist.');
+		}
+		
+		/**
+		* Sets values for Curl
+		* @param int $name
+		* @param mixed $value
+		* @return bool
+		*/
+		private function _setOpt($name, $value)
+		{
+			return ( bool ) curl_setopt($this->_curl, $name, $value);
+		}
+		
+		/**
+		* Sets multiple values for Curl
+		* @param array $value
+		* @return bool		
+		*/		
+		private function _setOptArray(array $value)
+		{
+			return ( bool ) curl_setopt_array($this->_curl, $value);
+		}
+		
+		/**
+		* Executes the curret resource
+		* @return mixed
+		*/
+		public function exec()
+		{
+			if(!$this->_isMulti){
+				$r = curl_exec($this->_curl);
+				
+				if(is_bool($r)){
+					return ( bool ) $r;
+				}
+				
+				return $r;			
+			}
+			
+			$running = null;
+			
+			do {
+			    curl_multi_exec($this->_curl, $running);
+			} while ($running > 0);			
+		}
+		
+		/**
+		* Used to add curls to the Multi CURL
+		* @param Curl $curl
+		* @return bool
+		*/
+		public function addHandle(Curl &$curl)
+		{
+			if ($this->_isMulti){
+				return ( bool ) (curl_multi_add_handle($this->_curl, $curl->getCurl()) === 0) ?: true;
+			}
+			
+			throw new Exception('Your using a Multi method yet you have defined a standard CURL, you should use $c = new Curl(true); for a Multi curl.');
+		}
+		
+		/**
+		* Used to remove curls from the Multi CURL
+		* @param Curl $curl
+		* @return bool
+		*/		
+		public function removeHandle(Curl &$curl)
+		{
+			if ($this->_isMulti){
+				return ( bool ) (curl_multi_remove_handle($this->_curl, $curl->getCurl()) === 0) ?: true;
+			}
+			
+			throw new Exception('Your using a Multi method yet you have defined a standard CURL, you should use $c = new Curl(true); for a Multi curl.');			
+			
+		}
+		
+		/**
+		* Used to get content from CURLs within Multi
+		* @param Curl $curl
+		* @return bool
+		*/		
+		public function getContent(Curl &$curl)
+		{
+			if ($this->_isMulti){
+				return curl_multi_getcontent($curl->getCurl());
+			}
+			
+			throw new Exception('Your using a Multi method yet you have defined a standard CURL, you should use $c = new Curl(true); for a Multi curl.');				
 		}
 		
 		/**
@@ -58,6 +164,18 @@
 		public function getVersion()
 		{
 			return ( array ) curl_version();
+		}
+		
+		/**
+		* Returns Information about the last CURL
+		* @param int $opt=null
+		* @return mixed 
+		* Full list of options here 
+		* http://www.php.net/manual/en/function.curl-getinfo.php
+		*/
+		public function getInfo($opt=null)
+		{
+			return curl_getinfo($this->_curl, $opt);
 		}
 		
 		/**
@@ -81,5 +199,10 @@
 			if ($this->_isResource) {
 				return curl_errno($this->_curl);
 			}
+		}
+		
+		public function getCurl()
+		{
+			return $this->_curl;
 		}
 	}
