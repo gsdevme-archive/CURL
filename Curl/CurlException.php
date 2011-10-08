93d5cbe9ebb0a88f0a2488020d62bffcc19a11e4
Gav <gavshouse90@gmail.com>
2011-10-08 16:46:08 +0100

    Curl Exception

---
+++ Curl/CurlException.php
@@ -0,0 +1,15 @@
+<?php
+
+    namespace Curl {
+        use \Exception as Exception;
+
+        /**
+         * @author Gavin Staniforth, http://twitter.com/gsphpdev
+         * @version 0.1
+         */
+        class CurlException extends Exception
+        {
+            
+        }
+
+    }
