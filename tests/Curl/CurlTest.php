9d433b1d6675243df617a764829a91eb5dd62d19
Gav <gavshouse90@gmail.com>
2011-10-08 16:45:26 +0100

    Added Unit tests for Curl.php

--- tests/Curl/CurlTest.php
+++ tests/Curl/CurlTest.php
@@ -1,7 +1,7 @@
 <?php
 
     use PHPUnit_Framework_TestCase as PHPUnit;
-use Cure\Curl as Curl;
+use Curl\Curl as Curl;
 
     class CurlTest extends PHPUnit
     {
@@ -10,12 +10,84 @@ use Cure\Curl as Curl;
 
         public function setup()
         {
-            $this->_curl = new Curl\Curl;
+            $this->_curl = new Curl;
         }
 
-        public function testInvoke()
+        public function testInstance()
         {
-            
+            $this->assertInstanceOf('Curl\Curl', $this->_curl);
+        }
+
+        public function testInstancePass()
+        {
+            $this->assertInstanceOf('Curl\Curl', new Curl(curl_init()));
+        }
+
+        public function testClone()
+        {
+            $this->assertInstanceOf('Curl\Curl', (clone $this->_curl));
+        }
+
+        public function testSetConstant()
+        {
+            try {
+                $this->_curl->CURLOPT_RETURNTRANSFER = true;
+            } catch (\Exception $e) {
+                $this->fail('Failed due to exception being thrown');
+            }
+        }
+
+        public function testCatchInvalidConstant()
+        {
+            try {
+                $this->_curl->CURLOPT_RETU = true;
+                $this->fail('No exception was thrown when an invalid property was set');
+            } catch (\Exception $e) {
+                
+            }
+        }
+
+        public function testFailedExec()
+        {
+            $this->assertEquals(false, $this->_curl->exec()); 
+        }
+
+        public function testExec()
+        {
+            $this->_curl->CURLOPT_URL = 'http://whatismyip.org';
+            $this->_curl->CURLOPT_RETURNTRANSFER = true;
+
+            $return = $this->_curl->exec();
+
+            if (!filter_var($return, FILTER_VALIDATE_IP)) {
+                $this->fail('Failed to validate IP from whatsismyip.org');
+            }
+        }
+
+        public function testExecCallback()
+        {
+            $this->_curl->CURLOPT_URL = 'http://whatismyip.org';
+            $this->_curl->CURLOPT_RETURNTRANSFER = true;
+
+            $return = $this->_curl->exec(function($return, $info) {
+                    return ( bool ) (filter_var($return, FILTER_VALIDATE_IP));
+                });
+
+            $this->assertTrue($return);
+        }
+
+        public function testExecInfo()
+        {
+            $this->_curl->CURLOPT_URL = 'http://whatismyip.org';
+            $this->_curl->CURLOPT_RETURNTRANSFER = true;
+
+            $return = $this->_curl->exec();
+
+            if ($this->_curl->getInfo('CURLINFO_HTTP_CODE') == '200') {
+                if (!filter_var($return, FILTER_VALIDATE_IP)) {
+                    $this->fail('Failed to validate IP from whatsismyip.org');
+                }
+            }
         }
 
     }
